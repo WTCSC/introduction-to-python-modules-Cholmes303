8ee828ec9db8a4a5b9d9429a5ea5db3da512ef57
Cholmes303 <cj@jasonholmes.net>
2024-09-10 14:12:22 -0600

    Finished and tested avg words

--- average.py
+++ average.py
@@ -10,3 +10,35 @@ format:
 
 "Average words per line: [average]"
 """
+
+#pulls text_utils file to count the words in sample.txt
+import text_utils as tu
+
+#creates function that defines average words of file
+def average_words(): 
+
+    #opens sample.txt file and reads
+    file = open('sample.txt', 'r')
+    
+    #translate file into text
+    text = file.read()
+
+    #returns the program reading the file back to the start when it does not have any lines left to read
+    file.seek(0)
+
+    #sets text equal to reading file lines
+    lines = len(file.readlines())
+
+    #sets words to word count of file
+    words = tu.count_words(text)
+
+    #divides words per text and floors with //
+    average = words//lines
+    
+    #closes file to stop running
+    file.close()
+
+    #returns average words in file per line
+    return f'Average words per line: {average}'
+
+print(average_words())
